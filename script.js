4bec16933c146cabcb4883692ed65e2b1962e37c
Colby Cardell <77302221+xXBricksquadXx@users.noreply.github.com>
2023-08-12 17:20:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+//get the geolocation of the user
+navigator.geolocation.getCurrentPosition((position) => {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
 
+  //update the content of the HTML elements with coordinates
+  document.getElementById('latitude').textContent= latitude;
+  document.getElementById('longitude').textContent= longitude;
+}, (Error) => {
+console.error (`Error getting geolocation: ${Error.message}`)  
+});
